bb20d2e08d3905f2e32528bd0f27880e198a479a
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2026-04-09 15:01:23 +0200

    fix: add oxfmt and oxlint

---
+++ nvim/.config/nvim/lsp/oxfmt.lua
@@ -0,0 +1,33 @@
+return {
+  cmd = function(dispatchers, config)
+    local cmd = 'oxfmt'
+    local local_cmd = (config or {}).root_dir and config.root_dir .. '/node_modules/.bin/oxfmt'
+    if local_cmd and vim.fn.executable(local_cmd) == 1 then
+      cmd = local_cmd
+    end
+    return vim.lsp.rpc.start({ cmd, '--lsp' }, dispatchers)
+  end,
+  filetypes = {
+    'javascript',
+    'javascriptreact',
+    'typescript',
+    'typescriptreact',
+    'toml',
+    'json',
+    'jsonc',
+    'json5',
+    'yaml',
+    'html',
+    'vue',
+    'handlebars',
+    'css',
+    'scss',
+    'less',
+    'graphql',
+    'markdown',
+  },
+  workspace_required = true,
+  root_dir = vim.fs.root(0, {
+    { '.oxfmtrc.json', '.oxfmtrc.jsonc', 'oxfmt.config.ts' }
+  })
+}
---
+++ nvim/.config/nvim/lsp/oxlint.lua
@@ -0,0 +1,21 @@
+return {
+  cmd = function(dispatchers, config)
+    local cmd = 'oxlint'
+    local local_cmd = (config or {}).root_dir and config.root_dir .. '/node_modules/.bin/oxlint'
+    if local_cmd and vim.fn.executable(local_cmd) == 1 then
+      cmd = local_cmd
+    end
+    return vim.lsp.rpc.start({ cmd, '--lsp' }, dispatchers)
+  end,
+  filetypes = {
+    'javascript',
+    'javascriptreact',
+    'typescript',
+    'typescriptreact',
+    'vue',
+    'svelte',
+    'astro',
+  },
+  root_markers = { '.oxlintrc.json', '.oxlintrc.jsonc', 'oxlint.config.ts' },
+  workspace_required = true,
+}
