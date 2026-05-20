7f5ae8f78744a39b79446e1f0ce15155ba702fc4
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2026-05-20 11:14:59 +0200

    fix: add git lineage keybind

--- nvim/.config/nvim/plugin/neogit.lua
+++ nvim/.config/nvim/plugin/neogit.lua
@@ -16,12 +16,16 @@ vim.keymap.set('n', '<leader>gg', function()
   vim.cmd('Neogit')
 end, { desc = 'Neogit' })
 
-vim.keymap.set('n', '<leader>gl', function()
-  vim.cmd('NeogitLogCurrent')
-end, { desc = 'Log of current file' })
-
 vim.keymap.set('v', '<leader>gl', function()
-  local start_line = vim.fn.line("'<")
-  local end_line = vim.fn.line("'>")
-  vim.cmd(start_line .. ',' .. end_line .. 'NeogitLogCurrent')
-end, { desc = 'Log of current selection' })
+  local start_line = vim.fn.line('v')
+  local end_line = vim.fn.line('.')
+  -- Ensure start_line is always the smaller number (if you selected bottom-up)
+  if start_line > end_line then
+    start_line, end_line = end_line, start_line
+  end
+
+  local file = vim.fn.expand('%:p')
+  local range_arg = start_line .. ',' .. end_line .. ':' .. file
+
+  require('neogit').action('log', 'log_current', { '-L', range_arg })()
+end, { desc = 'Neogit log of current selection' })
